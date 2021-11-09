b1947d65d70e1c8d9d6d758e030cbf2a810d430b
Alessandro Aprile <a.aprile@kevopartners.com>
2021-11-09 22:47:32 +0100

    add data text and data value components and use it in ShowData

--- src/components/showdata/ShowData.js
+++ src/components/showdata/ShowData.js
@@ -1,11 +1,13 @@
 import styles from "./ShowData.module.css";
-import globals from '../../App.module.css'
+import globals from '../../App.module.css';
+import DataText from '../data/DataText';
+import DataValue from '../data/DataValue';
 
 const ShowData = () => {
   return (
     <div className={`${styles.showdata_section} ${globals.centered_section}`}>
-      <p className={styles.datatext}>my phone number is</p>
-      <h3 className={styles.data}>(03)-22-48-30-73</h3>
+      <DataText text={"My phone number is"} />
+      <DataValue value="(03)-22-48-30-73"/>
     </div>
   );
 };
