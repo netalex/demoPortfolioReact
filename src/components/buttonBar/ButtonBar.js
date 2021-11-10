c33ce726b21a8cce64d0503dc827ebef5a9da316
Alessandro Aprile <a.aprile@kevopartners.com>
2021-11-10 10:50:08 +0100

    style icons

--- src/components/buttonBar/ButtonBar.js
+++ src/components/buttonBar/ButtonBar.js
@@ -1,26 +1,27 @@
 import styles from './ButtonBar.module.css';
+import globals from '../../App.module.css'
 
 const ButtonBar = () => {
   return (
     <div className={styles.button_section}>
         
-      <div className="button_bar centered_section">
-        <div className="button b1 centered_section">
+      <div className={`${styles.button_bar} ${globals.centered_section}`}>
+        <div className={`${styles.button} b1 ${globals.centered_section}`}>
           <i className="far fa-user"></i>
         </div>
-        <div className="button b2 centered_section">
+        <div className={`${styles.button} b2 ${globals.centered_section}`}>
           <i className="far fa-envelope"></i>
         </div>
-        <div className="button b3 centered_section">
+        <div className={`${styles.button} b3 ${globals.centered_section}`}>
           <i className="far fa-calendar-alt"></i>
         </div>
-        <div className="button b4 centered_section">
+        <div className={`${styles.button} b4 ${globals.centered_section}`}>
           <i className="fas fa-map-marked-alt"></i>
         </div>
-        <div className="button b5 centered_section">
+        <div className={`${styles.button} b5 ${globals.centered_section}`}>
           <i className="fas fa-phone-alt"></i>
         </div>
-        <div className="button b6 centered_section">
+        <div className={`${styles.button} b6 ${globals.centered_section}`}>
           <i className="fas fa-lock"></i>
         </div>
       </div>
